76af044b8a47079a4ded6eba3515d90103769dc9
dxd <dxd_work@163.com>
2017-07-28 13:10:35 +0800

    根据log删除股票tick数据导入中因为异常中断而冗余的数据

---
+++ DeleteSTRows2/DeleteSTRows2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using MySql.Data.MySqlClient;
+
+namespace DeleteSTRows2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FileStream fs = File.OpenRead(@"E:\股票tick数据导入完成记录\开始导入20170713 10-30-04.txt");
+            //StreamReader sr = new StreamReader(fs, Encoding.UTF8);
+
+            //string line = null;
+            //List<string> stockcodes = new List<string>();
+            //List<string> marketid = new List<string>();
+
+            //while ((line = sr.ReadLine()) != null)
+            //{
+            //    if (line.Contains("csv"))
+            //    {
+            //        marketid.Add(line.Substring(27, 2));
+            //        stockcodes.Add(line.Substring(29, 6));
+            //    }
+            //}
+            //fs.Close();
+            //sr.Close();
+
+            List<string> marketid = new List<string>();
+            List<string> stockcode = new List<string>();
+            marketid.Add("sz");
+            stockcode.Add("000554");
+
+            string connStr = "Server=192.168.2.129;User ID=root;Password=123456;Database=STHisDBTick2008;CharSet=utf8";
+            List<string> tableNames = new List<string>();
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "select TABLE_NAME from INFORMATION_SCHEMA.tables where TABLE_SCHEMA = \"sthisdbtick2008\"";
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    tableNames.Add(reader[0].ToString());
+                }
+            }
+
+            List<string> querys = new List<string>();
+            string query = "";
+            foreach(var tName in tableNames)
+            {
+                query = "DELETE FROM " + tName + " WHERE ";
+                for(int i=0;i<marketid.Count; ++i)
+                {
+                    if(i == marketid.Count-1)
+                    {
+                        query += string.Format("(marketid=\"{0}\" AND stockcode=\"{1}\");", marketid[i], stockcode[i]);
+                    }
+                    else
+                    {
+                        query += string.Format("(marketid=\"{0}\" AND stockcode=\"{1}\") OR ", marketid[i], stockcode[i]);
+                    }
+                }
+
+                querys.Add(query);
+            }
+
+            File.WriteAllLines(@"E:\股票tick数据删除处理语句.txt", querys);
+        }
+    }
+}
